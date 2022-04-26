1547ac838a5fc5ea54acf579f8f37d319bb01c1c
Gojart <gojarti_369@hotmail.com>
2022-04-25 20:40:41 -0700

    First Commit

---
+++ ConferenceDTO/Attendee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceDTO
+{
+    public class Attendee
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public virtual string FirstName { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public virtual string LastName { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string UserName { get; set; }
+
+        [StringLength(256)]
+        public virtual string EmailAddress { get; set; }
+
+    }
+}
---
+++ ConferenceDTO/SessionResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConferenceDTO
+{
+   public  class SessionResponse :Session
+    {
+        public Track Track { get; set; }
+
+        public List<Speaker> Speakers { get; set; } = new List<Speaker>();
+
+    }
+}
